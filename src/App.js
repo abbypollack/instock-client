d41f24d2a703904ba97cc332e478a94b47e832c3
Alec bana <anbana994@gmail.com>
2023-11-09 14:40:09 -0500

    style changes for waredeets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.scss';
-import AddInventoryItem from './pages/AddInventoryItem/AddInventoryItem';
-import AddNewWarehouse from './pages/AddNewWarehouse/AddNewWarehouse';
-import EditInventoryItem from './pages/EditInventoryItem/EditInventoryItem';
-import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
-import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
-// import InventoryPage from './pages/InventoryPage/InventoryPage';
-import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
-import WarehousePage from './pages/WarehousePage/WarehousePage';
-// import Header from './components/Header/Header.js';
+// import AddInventoryItem from './pages/AddInventoryItem/AddInventoryItem';
+// import AddNewWarehouse from './pages/AddNewWarehouse/AddNewWarehouse';
+// import EditInventoryItem from './pages/EditInventoryItem/EditInventoryItem';
+// import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
+// import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
+// // import InventoryPage from './pages/InventoryPage/InventoryPage';
+import WarehouseDetails from './components/WarehouseDetails/WarehouseDetails';
+// import WarehousePage from './pages/WarehousePage/WarehousePage';
+// // import Header from './components/Header/Header.js';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer.js';
+import Header from './components/Header/Header.js';
 import InventoryPage from "./pages/InventoryPage/InventoryPage.js";
 import EditInventoryItemPage from "./pages/EditInventoryItem/EditInventoryItem";
 
@@ -17,10 +18,10 @@ import EditInventoryItemPage from "./pages/EditInventoryItem/EditInventoryItem";
 function App() {
     return (
         <BrowserRouter>
-            {/* <Header /> */}
+            <Header />
             <Routes>
                 {/* <Route path="" element={<WarehousePage />} /> */}
-                {/* <Route path="" element={<WarehouseDetails />} /> */}
+                <Route path="/warehouseDetails" element={<WarehouseDetails />} />
                 <Route path="/inventory" element={<InventoryPage/>} />
                 {/* <Route path="" element={<InventoryItemDetails />} /> */}
                 {/* <Route path="" element={<EditWarehouse />} /> */}
