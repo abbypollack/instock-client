4cbe824c9b948c9d65c097b77e470acba8377c14
Alec bana <anbana994@gmail.com>
2023-11-09 12:02:04 -0500

    changes to the warehouse details

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AddNewWarehouse from './pages/AddNewWarehouse/AddNewWarehouse';
 import EditInventoryItem from './pages/EditInventoryItem/EditInventoryItem';
 import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
 import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
-import InventoryPage from './pages/InventoryPage/InventoryPage';
+// import InventoryPage from './pages/InventoryPage/InventoryPage';
 import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
 import WarehousePage from './pages/WarehousePage/WarehousePage';
 // import Header from './components/Header/Header.js';
@@ -17,7 +17,7 @@ import EditInventoryItemPage from "./pages/EditInventoryItem/EditInventoryItem";
 function App() {
     return (
         <BrowserRouter>
-            <Header />
+            {/* <Header /> */}
             <Routes>
                 {/* <Route path="" element={<WarehousePage />} /> */}
                 {/* <Route path="" element={<WarehouseDetails />} /> */}
--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,40 +1,15 @@
 import './WarehouseDetails.scss';
+import 
 
 
 function WarehouseDetails({warehouseInfo}){
     
     return(
         // <Header/>
-        
-        <section className='war-details'>
-
-            <section>
-                <h1 className=''>{}</h1>
-            </section>
-
-            <section className=''>
-                
-                <section className=''>
-                    <p className=''>By {}</p>
-                    <p className=''>{new Date().toLocaleDateString()}</p>
-                </section>
-                <section className=''>
-                    <div className=''>
-                        <img className='' src={} alt="icon"/>
-                        <p className=''>{}</p>
-                    </div>
-                    <div className=''>
-                        <img className='' src={} alt="icon"/>
-                        <p className=''>{}</p>
-                    </div>
-                </section>
-
-            </section>
-
-            <section>
-                <p className='info__description'>{}</p>
-            </section>
+        <section>
+            <h1>Warehousestitle</h1>
         </section>
+        
     )
 }
 
