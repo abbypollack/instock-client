cb9112299ba943aa105972ec63f3a46c841bd943
Jorge Perez <georgeaperez2000@gmail.com>
2023-11-09 17:51:02 -0500

    review of app.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import AddInventoryItem from './pages/AddInventoryItem/AddInventoryItem';
 // import EditInventoryItem from './pages/EditInventoryItem/EditInventoryItem';
 // import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
 // import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
-<<<<<<< HEAD
 import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage';
 import WarehousePage from './pages/WarehousePage/WarehousePage';
-=======
-import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage.js';
-// import WarehousePage from './pages/WarehousePage/WarehousePage';
 import Header from './components/Header/Header';
->>>>>>> develop
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer.js';
 import InventoryPage from "./pages/InventoryPage/InventoryPage.js";
 import EditInventoryItemPage from "./pages/EditInventoryItem/EditInventoryItem";
-
+//
 
 
 function App() {
@@ -32,11 +27,9 @@ function App() {
                 {/* <Route path="" element={<EditWarehouse />} /> */}
                 <Route path="/inventory/edit" element={<EditInventoryItemPage/>} />
                 {/* <Route path="" element={<AddNewWarehouse />} /> */}
-<<<<<<< HEAD
                 {/* <Route path="/inventory/add" element={<AddInventoryItemPage />} /> */}
-=======
+                
                 <Route path="/inventory/add" element={<AddInventoryItem />} />
->>>>>>> develop
                 </Routes>
             <Footer />
         </BrowserRouter> 
