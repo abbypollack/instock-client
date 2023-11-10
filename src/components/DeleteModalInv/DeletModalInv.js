884d3e25c5be94e6f3e88ef20a4b419e14a37842
Alec bana <anbana994@gmail.com>
2023-11-10 17:45:14 -0500

    pulling out to merge updates

---
+++ src/components/DeleteModalInv/DeletModalInv.js
@@ -0,0 +1,12 @@
+import './DeletModalInv';
+
+
+function DeletModalInv() {
+    return (
+        <section className="add-warehouse-section">
+            
+        </section>
+    );
+}
+
+export default DeletModalInv;
