37782a0eee7c6b7417075b6b1e7c74125420c0a0
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-10 11:48:11 -0500

    commit before PR

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.scss';
 import AddInventoryItem from './pages/AddInventoryItem/AddInventoryItem';
 // import AddNewWarehouse from './pages/AddNewWarehouse/AddNewWarehouse';
-// import EditInventoryItem from './pages/EditInventoryItem/EditInventoryItem';
 // import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
 // import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
 import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage.js';
@@ -18,14 +17,13 @@ function App() {
         <BrowserRouter>
             <Header />
             <Routes>
-                {/* <Route path="" element={<WarehousePage />} /> */}
+                {/* <Route path="/warehouse" element={<WarehousePage />} /> */}
                 <Route path="/warehouse/details" element={<WarehouseDetailsPage />} />
-                {/* <Route path="" element={<WarehouseDetails />} /> */}
+                {/* <Route path="/warehouse/edit" element={<EditWarehouse />} /> */}
+                {/* <Route path="/warehouse/add" element={<AddNewWarehouse />} /> */}
                 <Route path="/inventory" element={<InventoryPage/>} />
-                {/* <Route path="" element={<InventoryItemDetails />} /> */}
-                {/* <Route path="" element={<EditWarehouse />} /> */}
+                {/* <Route path="/inventory/details" element={<InventoryItemDetails />} /> */}
                 <Route path="/inventory/edit" element={<EditInventoryItemPage/>} />
-                {/* <Route path="" element={<AddNewWarehouse />} /> */}
                 <Route path="/inventory/add" element={<AddInventoryItem />} />
                 </Routes>
             <Footer />
