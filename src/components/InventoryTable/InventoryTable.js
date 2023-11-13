194abe4e69c8ea13f761855fec99cfa7f38b6afa
Your Name <you@example.com>
2023-11-13 13:28:55 -0500

    routed everything available to the proper routes

--- src/components/InventoryTable/InventoryTable.js
+++ src/components/InventoryTable/InventoryTable.js
@@ -1,20 +1,28 @@
 import './InventoryTable.scss';
+import { useNavigate, NavLink } from 'react-router-dom';
 import deleted from '../../assets/icons/delete_outline-24px.svg'
 import edit from '../../assets/icons/edit-24px.svg'
 import sort from '../../assets/icons/sort-24px.svg'
 import chevron from '../../assets/icons/chevron_right-24px.svg'
 import '../GlobalTable/GlobalTable.scss';
-import {useState, useEffect} from 'react';
+import { useState } from 'react';
 import DeleteInventory from '../DeleteModalInv/DeletModalInv';
 // import delete from '../DeleteModalInv/DeletModalInv'; 
 
-function InventoryTable({inventories, warehouses}) {
+function InventoryTable({ inventories, warehouses }) {
     const [deleteModal, setDeleteModal] = useState(false);
     const [selectedInventory, setSelectedInventory] = useState(null);
 
     const clickInventoryDelete = (inventories) => {
         setSelectedInventory(inventories);
         setDeleteModal(true);
+    }
+
+    const navigate = useNavigate();
+    const clickInventoryEdit = (inventoryEdit) => {
+        console.log(inventoryEdit)
+        setSelectedInventory(inventoryEdit);
+        navigate(`/inventory/edit/${inventoryEdit}`)
     }
     // const [items, setItems] = useState([]);
 
@@ -35,7 +43,7 @@ function InventoryTable({inventories, warehouses}) {
 
 
     return (
-    <div className="table">
+        <div className="table">
             <table className="table__container table__container--inventory">
                 <thead>
                     <tr>
@@ -48,27 +56,34 @@ function InventoryTable({inventories, warehouses}) {
                     </tr>
                 </thead>
                 <tbody>
-                {inventories.map((inventory) => (
-                            <tr key={inventory.id}>
-                                <td className="table__position1 table__item--inventory"><p>Inventory Item:</p>{inventory.item_name}<img src={chevron} alt="chevron"/></td>
-                                <td className="table__position2 table__item--inventory"><p>Category:</p>{inventory.category}</td>
-                                <td className="table__position3 table__item--inventory"><p>Status:</p>{inventory.status}</td>
-                                <td className="table__position4 table__item--inventory"><p>Quantity:</p>{inventory.quantity}</td>
-                                <td className="table__position5 table__item--inventory"><p>Warehouse:</p>{warehouses.find((warehouse) => warehouse.id === inventory.warehouse_id)?.warehouse_name || inventory.warehouse_name}</td>
-                                <td className="table__position6 table__item--inventory visibility-hidden"><p>Actions</p></td>
-                                <td className="table__position7 table__item--inventory"><img src={deleted} alt="deleted" onClick={() => clickInventoryDelete(inventory)}/></td>
-                                <td className="table__position8 table__item--inventory"><img src={edit} alt="edit" /></td>
-                                <td className="table__position9 table__item--inventory display-none__mobile">
-                                    <img  src={deleted} alt="deleted" onClick={() => clickInventoryDelete(inventory)} />
-                                    <img src={edit} alt="edit" />
-                                </td>
-                            </tr>
-                        ))}
-                    
+                    {inventories.map((inventory) => (
+                        <tr key={inventory.id}>
+                            <td className="table__position1 table__item--inventory">
+                                <NavLink to={`/inventory/${inventory.id}`}>
+                                    <p>Inventory Item:</p>{inventory.item_name}<img src={chevron} alt="chevron" />
+                                </NavLink>
+                            </td>
+                            <td className="table__position2 table__item--inventory"><p>Category:</p>{inventory.category}</td>
+                            <td className="table__position3 table__item--inventory"><p>Status:</p>{inventory.status}</td>
+                            <td className="table__position4 table__item--inventory"><p>Quantity:</p>{inventory.quantity}</td>
+                            <td className="table__position5 table__item--inventory"><p>Warehouse:</p>{warehouses.find((warehouse) => warehouse.id === inventory.warehouse_id)?.warehouse_name || inventory.warehouse_name}</td>
+                            <td className="table__position6 table__item--inventory visibility-hidden"><p>Actions</p></td>
+                            <td className="table__position7 table__item--inventory"><img src={deleted} alt="deleted" onClick={() => clickInventoryDelete(inventory)} /></td>
+                            <td className="table__position8 table__item--inventory"><img src={edit} alt="edit"
+                                onClick={() => clickInventoryEdit(inventory.id)} />
+                            </td>
+                            <td className="table__position9 table__item--inventory display-none__mobile">
+                                <img src={deleted} alt="deleted" onClick={() => clickInventoryDelete(inventory)} />
+                                <img src={edit} alt="edit"
+                                    onClick={() => clickInventoryEdit(inventory.id)} />
+                            </td>
+                        </tr>
+                    ))}
+
                 </tbody>
             </table>
             <section className='table__modal'>
-                    {deleteModal && <DeleteInventory openDelete={setDeleteModal} inventory={selectedInventory}/>}
+                {deleteModal && <DeleteInventory openDelete={setDeleteModal} inventory={selectedInventory} />}
             </section>
         </div>
     )
