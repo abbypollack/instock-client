0052550d9ea0417752c3749ee02b40c4be6f2b1d
Alec bana <anbana994@gmail.com>
2023-11-13 10:02:31 -0500

    merge  checkout to pull

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
@@ -1,28 +1,54 @@
 import './WarehouseDetailsPage.scss';
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import {  useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import edit from '../../assets/icons/edit-24px.svg';
-import arrowback from '../../assets/icons/arrow_back-24px.svg';
+import arrowBackIcon from '../../assets/icons/arrow_back-24px.svg';
+import editIcon from '../../assets/icons/edit-white-24px.svg';
 
 
+function WarehouseDetailsPage({warehouse}){
+    const [warehouseDetails, setwarehouseDetails] = useState(null);
+    const { warehouseId } = useParams();
+    const navigate = useNavigate();
 
-function WarehouseDetailsPage(){
+    const clickwarehouse = (warehouse) => {
+        setSelectedwarehouse(warehouse);
+    }
     
+    useEffect(() => {
+        const fetchwarehouseDetails = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8081/api/warehouses/${warehouseId}`);
+                setwarehouseDetails(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchwarehouseDetails();
+    }, [warehouseId]);
+
+    const handleEdit = () => {
+        navigate(`/warehouses/${warehouseId}`);
+    }
+
+    function handleCancel() {
+        navigate(-1);
+    }
     return(
         <>
         <section className="warehouse-inventory">
             <div className="warehouse-inventory__title-container">
             {/* <Link to="/warehouses"> */}
-                <img className="warehouse-inventory__title-icon" src={arrowback} alt="arrow back" />
+                <img className="warehouse-inventory__title-icon" src={arrowBackIcon} alt="arrow back" />
             {/* </Link> */}
-                <h1 className="warehouse-inventory__title">'Warehouse'</h1>
-                <button className="button__button button__edit"><img src={edit} alt="edit"/></button>
+                <h1 className="warehouse-inventory__title">{warehouseDetails?.warehouse_name}Washington</h1>
+                <button className="button__button button__edit"><img src={editIcon} onClick={handleCancel} alt="edit"/></button>
             </div>
             <section className="warehouse-inventory__subheader">
                 <section className='warehouse-inventory__box-1'>
                 <div className="warehouse-inventory__subheader--address">
-                    <h3 className='warehouse-inventory__'>Warehouse Address:</h3>
+                    <h3 className='warehouse-inventory__sub-title'>Warehouse Address:</h3>
                     <p className='warehouse-inventory__'>'JSX address'</p>
                     <p className='warehouse-inventory__'>'JSX city', 'state'</p>
                 </div>
@@ -30,12 +56,12 @@ function WarehouseDetailsPage(){
                 <section className='warehouse-inventory__box-2'>
                     <div className="warehouse-inventory__subheader--contact">
                     <div className="warehouse-inventory__subheader--contact-name">
-                        <h3 className='warehouse-inventory__'>Contact Name:</h3>
+                        <h3 className='warehouse-inventory__sub-title'>Contact Name:</h3>
                         <p className='warehouse-inventory__'>'JSX name'</p>
                         <p className='warehouse-inventory__'>' JSX position'</p>
                     </div>
                     <div className="warehouse-inventory__subheader--contact-info">
-                        <h3 className='warehouse-inventory__'>Contact Information:</h3>
+                        <h3 className='warehouse-inventory__sub-title'>Contact Information:</h3>
                         <p className='warehouse-inventory__'>'JSX phone'</p>
                         <p className='warehouse-inventory__'>'JSX email'</p>
                     </div>
