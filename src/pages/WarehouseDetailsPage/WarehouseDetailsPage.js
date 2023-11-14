94223d0d5c421990e4e3a547d2732ccc85b22d28
Kelly Schumacher <kschumacher93@gmail.com>
2023-11-13 19:24:03 -0500

    global list styling for tablet and stock tags

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
@@ -13,103 +13,103 @@ import WarehouseDetailstable from "../../components/WarehouseDetailsTable/Wareho
 // import InventoryTable from '../../components/InventoryTable/InventoryTable';
 
 function WarehouseDetailsPage({ warehouse }) {
-  const [warehouseDetails, setWarehouseDetails] = useState();
-  const { warehouseId } = useParams();
-  const navigate = useNavigate();
+    const [warehouseDetails, setWarehouseDetails] = useState();
+    const { warehouseId } = useParams();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchWarehouseDetails = async () => {
-      try {
-        console.log(warehouseId);
-        const response = await axios.get(
-          `http://localhost:8081/api/warehouses/${warehouseId}`
-        );
-        // console.log(response.data)
-        setWarehouseDetails(response.data);
-      } catch (error) {
-        console.error("this is the error: ", error);
-      }
+    useEffect(() => {
+        const fetchWarehouseDetails = async () => {
+        try {
+            console.log(warehouseId);
+            const response = await axios.get(
+            `http://localhost:8081/api/warehouses/${warehouseId}`
+            );
+            // console.log(response.data)
+            setWarehouseDetails(response.data);
+        } catch (error) {
+            console.error("this is the error: ", error);
+        }
+        };
+
+        fetchWarehouseDetails();
+    }, [warehouseId]);
+
+    const handleEdit = () => {
+        navigate(`/warehouses/${warehouseId}`);
     };
 
-    fetchWarehouseDetails();
-  }, [warehouseId]);
-
-  const handleEdit = () => {
-    navigate(`/warehouses/${warehouseId}`);
-  };
-
-  function handleCancel() {
-    navigate(-1);
-  }
-  return (
-    <>
-      <section className="warehouse-inventory">
-        <div className="warehouse-inventory__title-container">
-          <div className="warehouse-inventory__block-1">
-            {/* <Link to="/warehouses"> */}
-            <img
-              className="warehouse-inventory__title-icon"
-              src={arrowBackIcon}
-              alt="arrow back"
-            />
-            {/* </Link> */}
-            <h1 className="warehouse-inventory__title">
-              {warehouseDetails?.warehouse_name}
-            </h1>
-          </div>
-          <button className="button__button">
-            <img src={editIcon} onClick={handleEdit} alt="edit" />
-            <span className="button__edit">Edit</span>
-          </button>
-        </div>
-        <section className="warehouse-inventory__subheader">
-          <section className="warehouse-inventory__box-1">
-            <div className="warehouse-inventory__subheader--address">
-              <h3 className="warehouse-inventory__sub-title">
-                Warehouse Address:
-              </h3>
-              <p className="warehouse-inventory__address">
-                {warehouseDetails?.address}
-              </p>
-              <p className="warehouse-inventory__address">
-                {warehouseDetails?.city}, {warehouseDetails?.state}{" "}
-                {warehouseDetails?.country}
-              </p>
+    function handleCancel() {
+        navigate(-1);
+    }
+    return (
+        <>
+        <section className="warehouse-inventory">
+            <div className="warehouse-inventory__title-container">
+            <div className="warehouse-inventory__block-1">
+                {/* <Link to="/warehouses"> */}
+                <img
+                className="warehouse-inventory__title-icon"
+                src={arrowBackIcon}
+                alt="arrow back"
+                />
+                {/* </Link> */}
+                <h1 className="warehouse-inventory__title">
+                {warehouseDetails?.warehouse_name}
+                </h1>
             </div>
-          </section>
-          <section className="warehouse-inventory__box-2">
-            <div className="warehouse-inventory__subheader--contact">
-              <div className="warehouse-inventory__subheader--contact-name">
-                <h3 className="warehouse-inventory__sub-title">
-                  Contact Name:
-                </h3>
-                <p className="warehouse-inventory__">
-                  {warehouseDetails?.contact_name}
-                </p>
-                <p className="warehouse-inventory__">
-                  {warehouseDetails?.contact_position}
-                </p>
-              </div>
-              <div className="warehouse-inventory__subheader--contact-info">
-                <h3 className="warehouse-inventory__sub-title">
-                  Contact Information:
-                </h3>
-                <p className="warehouse-inventory__">
-                  {warehouseDetails?.contact_phone}
-                </p>
-                <p className="warehouse-inventory__">
-                  {warehouseDetails?.contact_email}
-                </p>
-              </div>
+            <button className="button__button">
+                <img src={editIcon} onClick={handleEdit} alt="edit" />
+                <span className="button__edit">Edit</span>
+            </button>
             </div>
-          </section>
+            <section className="warehouse-inventory__subheader">
+            <section className="warehouse-inventory__box-1">
+                <div className="warehouse-inventory__subheader--address">
+                <h3 className="warehouse-inventory__sub-title">
+                    Warehouse Address:
+                </h3>
+                <p className="warehouse-inventory__address">
+                    {warehouseDetails?.address}
+                </p>
+                <p className="warehouse-inventory__address">
+                    {warehouseDetails?.city}, {warehouseDetails?.state}{" "}
+                    {warehouseDetails?.country}
+                </p>
+                </div>
+            </section>
+            <section className="warehouse-inventory__box-2">
+                <div className="warehouse-inventory__subheader--contact">
+                <div className="warehouse-inventory__subheader--contact-name">
+                    <h3 className="warehouse-inventory__sub-title">
+                    Contact Name:
+                    </h3>
+                    <p className="warehouse-inventory__">
+                    {warehouseDetails?.contact_name}
+                    </p>
+                    <p className="warehouse-inventory__">
+                    {warehouseDetails?.contact_position}
+                    </p>
+                </div>
+                <div className="warehouse-inventory__subheader--contact-info">
+                    <h3 className="warehouse-inventory__sub-title">
+                    Contact Information:
+                    </h3>
+                    <p className="warehouse-inventory__">
+                    {warehouseDetails?.contact_phone}
+                    </p>
+                    <p className="warehouse-inventory__">
+                    {warehouseDetails?.contact_email}
+                    </p>
+                </div>
+                </div>
+            </section>
+            </section>
+            <div>
+            <WarehouseDetailstable />
+            </div>
         </section>
-        <div>
-          <WarehouseDetailstable />
-        </div>
-      </section>
-    </>
-  );
+        </>
+    );
 }
 
 export default WarehouseDetailsPage;
